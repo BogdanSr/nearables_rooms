4714fd589b693130165c6eb1f306257660573c9e
Bogdan Shubravyi <Bogdan.Shubravyi@eleks.com>
2015-05-21 18:22:47 +0300

    Initial

---
+++ Rooms/Estimote-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Estimote-Bridging-Header.h
+//  Rooms
+//
+//  Created by shubravyi.b@gmail.com on 21/5/2015.
+//  Copyright (c) 2015 shubravyi.b@gmail.com. All rights reserved.
+//
+
+#import <EstimoteSDK/EstimoteSDK.h>
---
+++ Rooms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rooms
+//
+//  Created by shubravyi.b@gmail.com on 21/5/2015.
+//  Copyright (c) 2015 shubravyi.b@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
